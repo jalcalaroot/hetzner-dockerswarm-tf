3e18aebc8afdb502c371f65f836b10ace8edaf6f
johan alcala <jalcala@droptek.com>
2019-04-12 17:44:05 -0300

    actualizando

--- servers/deploy.sh
+++ servers/deploy.sh
@@ -5,7 +5,7 @@ apt-get -y install \
   apt-transport-https \
   ca-certificates \
   curl
- 
+# Install docker 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
  
 add-apt-repository \
@@ -17,4 +17,11 @@ apt-get update
  
 apt-get -y install docker-ce
 
-docker run --name nginxserver --restart unless-stopped -p 80:80 -d nginx
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+# deploy dockerui
+
+docker run --restart unless-stopped -d -p 9000:9000 -v /var/run/docker.sock:/docker.sock --name dockerui abh1nav/dockerui:latest -e="/docker.sock"
--- servers/servers.tf
+++ servers/servers.tf
@@ -1,19 +1,46 @@
+### ingresar token
 variable "hcloud_token" {}
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = "${var.hcloud_token}"
 }
-#  Main ssh key
+###----------------------------
+# crear key
 resource "hcloud_ssh_key" "default" {
   name       = "main ssh key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMB5nWb9EnoI73dTW4Lt8q5NoDDImkB6XlNE5UYr52QDDFexWhkWeHTA8OiaGme3qDqgvbwyRDLEIXxZNGxBOuIYK31hysYHkwKnWcZXb2wJAgrqBDw0F5KYX9z3cBRltnM1NcxRhF8wzb9Sg2Akhu2fEEiZuIY2ds8tr+rIv3xoZ2GIh6k++z8Zyl6ssw8fI/1aBNuSBp2pGBQzXFG1D/JUhXkuPgD+fVNgTW2cNxIRqrUO6fftUR9x19AIoqlRZYYY+ywYs7jpW7v7MLtae+5KK/l5RbDHAVOqSV4jfj517NIeGZxIlAswpqwNKOT2LmOkb9OIZauoSWV4yjkywT jalcalaroot@jalcalaroot-ThinkPad-T450"
 }
-resource "hcloud_server" "testnode1" {
+###----------------------------
+resource "hcloud_server" "server1" {
   name        = "server1"
   image       = "ubuntu-16.04"
   server_type = "cx11"
   ssh_keys    = ["${hcloud_ssh_key.default.name}"]
+  user_data    = "${file("deploy.sh")}"
 }
-output "public_ip4" {
-  value = "${hcloud_server.testnode1.ipv4_address}"
+output "public_ip4-1" {
+  value = "${hcloud_server.server1.ipv4_address}"
 }
+###----------------------------
+resource "hcloud_server" "server2" {
+  name        = "server2"
+  image       = "ubuntu-16.04"
+  server_type = "cx11"
+  ssh_keys    = ["${hcloud_ssh_key.default.name}"]
+  user_data    = "${file("deploy.sh")}"
+}
+output "public_ip4-2" {
+  value = "${hcloud_server.server2.ipv4_address}"
+}
+###----------------------------
+resource "hcloud_server" "server3" {
+  name        = "server3"
+  image       = "ubuntu-16.04"
+  server_type = "cx11"
+  ssh_keys    = ["${hcloud_ssh_key.default.name}"]
+  user_data    = "${file("deploy.sh")}"
+}
+output "public_ip4-3" {
+  value = "${hcloud_server.server3.ipv4_address}"
+}
+
